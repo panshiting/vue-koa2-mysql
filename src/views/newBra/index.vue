010cb35ae7e4b917ec25113ec9c5580c17372635
panshiting <1030932605@qq.com>
2019-01-28 15:37:00 +0800

    添加新内容

---
+++ src/views/newBra/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>index</section>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data () {
+    return {}
+  },
+  created () {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+</style>
